aef8d5efaf562219e2f19969846601208e412884
mondayy1 <hojoong310@gmail.com>
2025-12-03 10:14:01 +0900

    feat: add mock with mockery

--- apps/backend/internal/db/db.go
+++ apps/backend/internal/db/db.go
@@ -12,7 +12,7 @@ func NewDBConnection(cfg *config.Config) (*sql.DB, error) {
 	//DSN Create
 	dsn := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
-		cfg.DB.Host, cfg.DB.Port, cfg.DB.User, cfg.DB.Password, cfg.DB.Name, cfg.DB.SSLMode,
+		cfg.DB.Host, cfg.DB.Port, cfg.DB.User, cfg.DB.Password, cfg.DB.DBName, cfg.DB.SSLMode,
 	)
 
 	db, err := sql.Open("postgres", dsn)
@@ -25,7 +25,7 @@ func NewDBConnection(cfg *config.Config) (*sql.DB, error) {
 	db.SetConnMaxLifetime(5 * time.Minute)
 
 	if err := db.Ping(); err != nil {
-		return nil, fmt.Errorf("failed to ping database (host: %s, db: %s): %w", cfg.DB.Host, cfg.DB.Name, err)
+		return nil, fmt.Errorf("failed to ping database (host: %s, db: %s): %w", cfg.DB.Host, cfg.DB.DBName, err)
 	}
 
 	return db, nil
