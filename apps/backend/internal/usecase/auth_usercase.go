4a3bbc6b97c0df812720a2cc5fabea7ef11e5150
mondayy1 <hojoong310@gmail.com>
2025-12-16 09:59:34 +0900

    feat: add login method

--- apps/backend/internal/usecase/auth_usercase.go
+++ apps/backend/internal/usecase/auth_usercase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"context"
 	"crypto/rsa"
+	"errors"
 
 	"github.com/everyday-studio/ollm/internal/config"
 	"github.com/everyday-studio/ollm/internal/domain"
@@ -25,4 +26,26 @@ func (uc *authUseCase) SignUpUser(ctx context.Context, user *domain.User) (*doma
 	user.Password = hashedPassword
 
 	return uc.userRepo.Save(ctx, user)
+}
+
+func (uc *authUseCase) Login(ctx context.Context, email string, password string) (*domain.LoginResponse, error) {
+	user, err := uc.userRepo.GetUserByEmail(ctx, email)
+	if err != nil {
+		return nil, err
+	}
+
+	match, err := security.ComparePasswordHash(password, user.Password)
+	if err != nil {
+		return nil, err
+	}
+	if !match {
+		return nil, errors.New("invalid credentials")
+	}
+
+	return uc.generateTokens(user)
+}
+
+func (uc *authUseCase) generateTokens(user *domain.User) (*domain.LoginResponse, error) {
+	response := &domain.LoginResponse{}
+	return response, nil
 }
